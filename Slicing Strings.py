4672b33cf2158fd7210d157cbed0922d3e39e057
Siavash-Ghorbani <siavash_vb2007@yahoo.com>
2023-08-30 18:08:33 +0330

    Slicing String

---
+++ Slicing Strings.py
@@ -0,0 +1,15 @@
+#Slicing
+strMyText = "Hello Canada!"
+print(strMyText[6:10])
+
+#Slice From the Start
+strMyText = "Hello Canada!"
+print(strMyText[:5])
+
+#Slice To the End
+strMyText = "Hello Canada!"
+print(strMyText[3:])
+
+#Negative Indexing
+strMyText = "Hello Canada!"
+print(strMyText[-5:-2])
