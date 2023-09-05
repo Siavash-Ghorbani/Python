19f305dd58b41332d1d1e38c9914f96719274664
Siavash-Ghorbani <siavash_vb2007@yahoo.com>
2023-09-05 18:36:31 +0330

    Loop Lists

---
+++ Loop Lists.py
@@ -0,0 +1,26 @@
+lstWeekDays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+
+# for x in lstWeekDays:
+#     print(x)
+#
+# [print(x) for x in lstWeekDays]
+#
+# for x in range(len(lstWeekDays)):
+#     print(lstWeekDays[x])
+
+# for x in range(2,len(lstWeekDays)):
+#     print(lstWeekDays[x])
+
+# for x in range(-2,3):
+#     print(lstWeekDays[x])
+
+# for x in range(-3,2,3):
+#     print(lstWeekDays[x])
+
+# for x in range(0,20,2):
+#     print(x)
+
+i = 0
+while i < len(lstWeekDays):
+    print(lstWeekDays[i])
+    i += 1
